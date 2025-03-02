neoelec/at91sam7s-ek

commit 6d24f574239a899bdaaef1f76d09c9ca0b6d2d96

utility/stdio: fix build errors
Change list:
1. This commit fixes following build errors.

at91lib/utility/stdio.c:64:8: error: missing initializer for field '_inc' of 'struct _reent' [-Werror=missing-field-initializers]
   64 | struct _reent r = {0, (FILE *) 0, (FILE *) 1, (FILE *) 0};
      |        ^~~~~~
In file included from /usr/include/newlib/stdio.h:60,
                 from at91lib/utility/stdio.c:49:
/usr/include/newlib/sys/reent.h:587:8: note: '_inc' declared here
  587 |   int  _inc;                    /* used by tmpnam */
      |        ^~~~
at91lib/utility/stdio.c: In function 'vsnprintf':
at91lib/utility/stdio.c:306:34: error: comparison of integer expressions of different signedness: 'int' and 'size_t' {aka 'unsigned int'} [-Werror=sign-compare]
  306 |     while (*pFormat != 0 && size < length) {
      |                                  ^
at91lib/utility/stdio.c:343:30: error: comparison of integer expressions of different signedness: 'int' and 'size_t' {aka 'unsigned int'} [-Werror=sign-compare]
  343 |             if (size + width > length) {
      |                              ^
at91lib/utility/stdio.c:368:14: error: comparison of integer expressions of different signedness: 'int' and 'size_t' {aka 'unsigned int'} [-Werror=sign-compare]
  368 |     if (size < length) {
      |              ^
cc1: all warnings being treated as errors

Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

packages/at91lib/utility/stdio.c
@@ -61,7 +61,11 @@
 //------------------------------------------------------------------------------
 
 // Required for proper compilation.
-struct _reent r = {0, (FILE *) 0, (FILE *) 1, (FILE *) 0};
+struct _reent r = {
+    ._stdin = (FILE *) 0,
+    ._stdout = (FILE *) 1,
+    ._stderr = (FILE *) 0,
+};
 struct _reent *_impure_ptr = &r;
 
 //------------------------------------------------------------------------------
@@ -294,7 +298,7 @@ signed int vsnprintf(char *pStr, size_t length, const char *pFormat, va_list ap)
     char          fill;
     unsigned char width;
     signed int    num = 0;
-    signed int    size = 0;
+    size_t        size = 0;
 
     // Clear the string
     if (pStr) {

commit 064b6b97a09fe56c257fa9d9f129827ae17b86bb

utility:stdio: fix bug, print does not work
Change list:
1. Bug is introduced after commit 6d24f574239a899bdaaef1f76d09c9ca0b6d2d96
2. Use correct data type for index.

Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

packages/at91lib/utility/stdio.c
@@ -295,10 +295,10 @@ signed int PutHexa(
 //------------------------------------------------------------------------------
 signed int vsnprintf(char *pStr, size_t length, const char *pFormat, va_list ap)
 {
-    char          fill;
-    unsigned char width;
-    signed int    num = 0;
-    size_t        size = 0;
+    char fill;
+    size_t width;
+    size_t num = 0;
+    size_t size = 0;
 
     // Clear the string
     if (pStr) {